81584c5ae10e3fc71fe7c2f83b7729d345d0d08e
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2023-08-20 12:52:47 +0530

    logout
    Signed-off-by: Gopisakthivel <gopisakthivel@alphabsolutions.com>

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { sidebarLinks } from "@/constants";
 import { usePathname, useRouter } from 'next/navigation';
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
 
 
 
@@ -36,6 +37,24 @@ const  LeftSidebar=()=> {
             </Link>
           );
         })}
+      </div>
+
+      <div className='mt-10 px-6'>
+
+      <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className='flex cursor-pointer gap-4 p-4'>
+              <Image
+                src='/assets/logout.svg'
+                alt='logout'
+                width={24}
+                height={24}
+              />
+
+              <p className='text-light-2 max-lg:hidden'>Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
 
         </section>
