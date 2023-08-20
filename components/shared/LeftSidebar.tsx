cbae095f66d526febe2afcab7cfc0f84c2ec0164
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2023-08-20 12:27:54 +0530

    SideBar
    Signed-off-by: Gopisakthivel <gopisakthivel@alphabsolutions.com>

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,23 +1,29 @@
 
-
+'use client'
 import Link from 'next/link'
 
 
 import Image from 'next/image'
 import { sidebarLinks } from "@/constants";
+import { usePathname, useRouter } from 'next/navigation';
 
 
 
 const  LeftSidebar=()=> {
+
+    const router = useRouter();
+    const pathname=usePathname()
     return(
         <section className='custom-scrollbar leftsidebar'>
             <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {sidebarLinks.map((link) => {
+
+            const isActive=(pathname.includes(link.route) && link.route.length>1 || pathname===link.route)
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link `}
+              className={`leftsidebar_link ${isActive && 'bg-primary-500'} `}
             >
               <Image
                 src={link.imgURL}
